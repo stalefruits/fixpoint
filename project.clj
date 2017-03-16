d933e59e9bb3d62f165f6e1ae3d76baae5959fc1
Yannick Scherer <yannick.scherer@stylefruits.de>
2017-03-16 09:51:12 +0100

    add 'codox' plugin.

--- project.clj
+++ project.clj
@@ -15,9 +15,19 @@
           [org.postgresql/postgresql "9.4.1212"]
           [mysql/mysql-connector-java "5.1.41"]
           [cc.qbits/spandex "0.3.4"
-           :exclusions [org.clojure/clojure]]]}}
+           :exclusions [org.clojure/clojure]]]}
+   :codox {:dependencies [[org.clojure/tools.reader "1.0.0-beta4"]
+                          [codox-theme-rdash "0.1.1"]]
+           :plugins [[lein-codox "0.10.3"]]
+           :codox {:project {:name "fixpoint"}
+                   :metadata {:doc/format :markdown}
+                   :themes [:rdash]
+                   :source-paths ["src"]
+                   :source-uri "https://github.com/stylefruits/fixpoint/blob/master/{filepath}#L{line}"
+                   :namespaces [fixpoint.core #"^fixpoint\.datasource\..*"]}}}
   :test-selectors {:default    #(not-any? % [:elastic :mysql :postgresql])
                    :elastic    :elastic
                    :mysql      :mysql
                    :postgresql :postgresql}
+  :aliases {"codox" ["with-profile" "codox,dev" "codox"]}
   :pedantic? :abort)
