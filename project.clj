4ad29c7e1d46896e8e1ba79c69b9d52a13b2e035
Yannick Scherer <yannick.scherer@gmail.com>
2017-08-11 14:22:06 +0200

    Version 0.1.4-SNAPSHOT

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject stylefruits/fixpoint "0.1.3"
+(defproject stylefruits/fixpoint "0.1.4-SNAPSHOT"
   :description "Simple & Powerful Test Fixtures/Datasources for Clojure"
   :url "https://github.com/stylefruits/fixpoint"
   :license {:name "Apache License 2.0"
